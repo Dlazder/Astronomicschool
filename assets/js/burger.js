7afc615fec764bfd593ba77789b8f0932e0ed0f4
Dlazder <124579368+Dlazder@users.noreply.github.com>
2023-03-19 17:14:09 +0300

    Delete assets directory

--- assets/js/burger.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.burger__outer').onclick = function() {
-    document.querySelector('.burger').classList.toggle('active');
-    document.querySelector('.burger__nav').classList.toggle('hidden');
-    document.querySelector('.burger__outer').classList.toggle('opened');
-    body.classList.toggle('no-scroll');
-}
