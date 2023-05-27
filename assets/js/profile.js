25e0b9257a5dcf48dcacf006af7ebab32c29af84
Dlazder <124579368+Dlazder@users.noreply.github.com>
2023-05-27 13:29:25 +0300

    Delete assets directory

--- assets/js/profile.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".user-name").innerHTML = localStorage.getItem('userName');
-
-if (document.querySelector('input')) {
-    document.querySelector('input').value = localStorage.getItem('userName');
-    document.querySelector('.change').onclick = function() {
-        localStorage.setItem('userName', document.querySelector('input').value);
-        document.querySelector(".user-name").innerHTML = localStorage.getItem('userName');
-    }
-};
